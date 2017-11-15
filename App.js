45c0b773f32b92a89fcc881a980b0c6c77ecb0cf
Devon Bain <dwb264@cornell.edu>
2017-11-15 11:33:04 -0500

    added nested navigation and worked on passing props between screens

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Button, StyleSheet, Text, TextInput, ScrollView, View, Alert, FlatList, Image, TouchableHighlight, TouchableOpacity} from 'react-native';
-import { StackNavigator } from 'react-navigation';
+import { StackNavigator, TabNavigator, TabView} from 'react-navigation';
 import DateTimePicker from 'react-native-modal-datetime-picker';
 
 var userType; // either mover or requester
@@ -164,7 +164,7 @@ class LoginScreen extends React.Component {
                 <Button
                     onPress={() => {
                         if (userType == 'requester' && validateLogin) {
-                            navigate('RequestForm');
+                            navigate('Requester');
                         } else if (userType == 'mover' && validateLogin){
                             navigate('AllJobsScreen');
                         }
@@ -212,7 +212,7 @@ class GetCodeScreen extends React.Component {
                 <Button
                     onPress={() => {
                         if (userType == 'requester') {
-                            navigate('RequestForm');
+                            navigate('Requester');
                         } else {
                             navigate('AllJobsScreen');
                         }
@@ -224,8 +224,6 @@ class GetCodeScreen extends React.Component {
         );
     }
 }
-
-
 
 class EnterCodeScreen extends React.Component {
 
@@ -256,7 +254,7 @@ class EnterCodeScreen extends React.Component {
                 <Button
                     onPress={() => {
                         if (userType == 'requester' && validateCode) {
-                            navigate('RequestForm');
+                            navigate('Requester');
                         }
                     }}
                     title="Join"
@@ -267,10 +265,11 @@ class EnterCodeScreen extends React.Component {
     }
 }
 
+
 class RequestFormScreen extends React.Component {
 
     static navigationOptions = {
-        header: null
+        tabBarLabel: 'Request',
     };
 
     constructor(props) {
@@ -341,8 +340,7 @@ class RequestFormScreen extends React.Component {
                 />
                 <View style={styles.containerTop}>
                     <View style={styles.grayHeader}>
-                        <Text style={styles.h1}>What do you need help with today?</Text>
-                        <Text style={{height: 20, width: "90%", margin:10}}>Describe the job and we&#8217;ll find you a mover!</Text>
+                        <Text style={styles.h1}>Submit a new job request</Text>
                     </View>
                     <TextInput
                         style={styles.input}
@@ -381,7 +379,7 @@ class RequestFormScreen extends React.Component {
                     <Text style={{height: 40, width: "90%", margin:10}}>📷 Upload Photos</Text>
                     <View style={styles.grayFooter}>
                         <Button
-                            onPress={() => validateForm() ? navigate('MoverList') : false}
+                            onPress={() => validateForm() ? navigate('Movers') : false}
                             title="Find Movers"
                             color="#00796B"
                         />
@@ -399,12 +397,15 @@ class MoverList extends React.Component {
         this.state = {
             data: [ // Hardcoded for now
                 { key: 0, values: {
-                    "id": 0, // Should correspond to id in database
+                    "id": 0,
                     "name": "Jeff McMover",
                     "photo": require("./img/jeff.png"),
                     "price": 400,
                     "startTime": new Date("Tue Mar 24 2015 20:00:00 GMT-0400 (EDT)"),
-                    "rating": 4.9
+                    "rating": 4.9,
+                    "phone": "555-867-5309",
+                    "vehicle": "Large box truck",
+                    "payment": "Cash, Venmo"
                     }
                 },
                 { key: 1, values: {
@@ -413,7 +414,10 @@ class MoverList extends React.Component {
                     "photo": require("./img/jeff.png"),
                     "price": 425,
                     "startTime": new Date("Tue Mar 24 2015 20:00:00 GMT-0400 (EDT)"),
-                    "rating": 4.8
+                    "rating": 4.8,
+                    "phone": "555-867-5311",
+                    "vehicle": "Small box truck",
+                    "payment": "Cash ONLY"
                     }
                 }
             ]
@@ -443,7 +447,7 @@ class MoverList extends React.Component {
                             <Text>${item.values.price} | Start at {item.values.startTime.getHours()}:{item.values.startTime.getMinutes()} | {item.values.rating}/5 Stars</Text>
                         </View>
                         <Text
-                            onPress={() => this.props.navigation.navigate("MoverDetail")}
+                            onPress={() => this.props.navigation.navigate("ViewMover", {moverInfo: item.values})}
                             style={{fontWeight: "bold", color: "#00796B"}}
                         >
                             Details
@@ -480,23 +484,17 @@ class MoverListScreen extends React.Component {
 
 MoverListScreen.router = MoverList.router; // Nested navigator: https://reactnavigation.org/docs/intro/nesting
 
+
 class MoverDetailScreen extends React.Component {
+
+    static navigationOptions = {
+        header: null
+    };
 
     constructor(props) {
         super(props);
         this.state = {
-            data: {
-                // Should get this from the database
-                "id": 0,
-                "name": "Jeff McMover",
-                "photo": require("./img/jeff.png"),
-                "price": 400,
-                "startTime": new Date("Tue Mar 24 2015 20:00:00 GMT-0400 (EDT)"),
-                "rating": 4.9,
-                "phone": "555-867-5309",
-                "vehicle": "Large box truck",
-                "payment": "Cash, Venmo"
-            },
+            data: this.props.navigation.state.params.moverInfo,
             preAccept: "flex",
             postAccept: "none"
         }
@@ -601,7 +599,7 @@ class ReviewScreen extends React.Component {
                 'Thank You!',
                 'You have rated ' + this.state.data.name.split(" ")[0] + ' ' + this.state.numStars + ' stars.',
                 [
-                    {text: 'Exit', onPress: () => navigate("GetCode")},
+                    {text: 'Exit', onPress: () => navigate("Requester")},
                 ],
                 { cancelable: true }
             );
@@ -679,14 +677,24 @@ const App = StackNavigator({
     Login: { screen: LoginScreen },
     GetCode: { screen: GetCodeScreen },
     EnterCode: { screen: EnterCodeScreen },
-    RequestForm: { screen: RequestFormScreen },
-    MoverList: { screen: MoverListScreen},
-    MoverDetail: { screen: MoverDetailScreen},
-    Review: { screen: ReviewScreen },
+    Requester: { screen: TabNavigator({
+        RequestForm: { screen: RequestFormScreen },
+        Movers: { screen: StackNavigator({
+            MoverList: { screen: MoverListScreen },
+            ViewMover: { screen: MoverDetailScreen},
+            Review: { screen: ReviewScreen },
+        }), navigationOptions: ({ navigation }) => ({
+            header: null,
+        }), },
+    }), navigationOptions: ({ navigation }) => ({
+        header: null,
+    }),
+    },
     },
     {
         headerMode: 'screen'
     });
+
 
 
 const styles = StyleSheet.create({
@@ -705,8 +713,7 @@ const styles = StyleSheet.create({
     grayHeader: {
         backgroundColor: "#e8e8e8",
         width: "100%",
-        paddingTop: 40,
-        paddingBottom: 20,
+        padding: 20,
         flex: 0,
         alignItems: 'center',
         marginBottom: 20,
